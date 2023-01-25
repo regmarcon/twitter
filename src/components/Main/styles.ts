6e11c541998ca96c62580c8cbe693ca440d28540
Regiane Marcon <regmarcon21@gmail.com>
2023-01-25 17:05:28 -0400

    Ajustando styles do componente Main

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -11,6 +11,7 @@ export const Container = styled.div`
     }
 `;
 export const Header = styled.div`
+   z-index: 2;
    position: sticky;
    top: 0;
    background: var(--primary);
@@ -35,8 +36,22 @@ export const Header = styled.div`
    }
 `;
 export const BackIcon = styled(ArrowLeft)`
+    width: 24px;
+    height: 24px;
+    fill: var(--twitter);
     
 `;
 export const ProfileInfo = styled.div`
+    margin-left: 17px;
+    display: flex;
+    flex-direction: column;
+    > strong{
+        font-size: 19px;
+    }
+    > span{
+        font-size: 15px;
+        color: var(--gray);
+    }
+
    
 `;
