1c3af33d778d84fd544cd5c2d72e56a4fa29281b
Regiane Marcon <regmarcon21@gmail.com>
2023-01-31 16:53:32 -0400

    atualizacao do MenuBar

---
+++ src/components/MenuBar/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Container,Topside,Logo,MenuButton,HomeIcon,BellIcon,EmailIcon,FavoriteIcon,
+    ProfileIcon, ProfileData,Botside,Avatar } from './styles';
+import Button from '../Button';
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+        <Topside>
+            <Logo />
+            <MenuButton>
+                <HomeIcon /><span>Página Inicial</span>                
+            </MenuButton>    
+            <MenuButton>
+                <BellIcon /><span>Notificações</span>                
+            </MenuButton>   
+            <MenuButton>
+                <EmailIcon /><span>Mensagens</span>                
+            </MenuButton>   
+            <MenuButton>
+                <FavoriteIcon /><span>Favoritados</span>                
+            </MenuButton>   
+            <MenuButton>
+                <ProfileIcon /><span className='ative'>Perfil</span>                
+            </MenuButton>   
+            <Button><span>Tweetar</span></Button>    
+        </Topside>
+        <ProfileData>
+
+        </ProfileData>
+    </Container>
+  );
+}
+
+export default MenuBar;
